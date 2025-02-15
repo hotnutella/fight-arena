3d1ce244b984e9b82845a4e549156b8dde8f6f93
Deniss Suhhanov <desuhh@taltech.ee>
2025-02-15 23:18:00 +0200

    Game boilerplate

---
+++ src/objects/Player.ts
@@ -0,0 +1,57 @@
+export class Player extends Phaser.Physics.Arcade.Sprite
+{
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
+    constructor (scene: Phaser.Scene, x: number, y: number)
+    {
+        super(scene, x, y, 'cat');
+
+        this.setOrigin(0.5, 0.5);
+        this.scale = 2;
+
+        this.scene.add.existing(this);
+        this.scene.physics.add.existing(this);
+
+        if (scene.input.keyboard) {
+            this.cursors = scene.input.keyboard.createCursorKeys();
+        } else {
+            throw new Error('Keyboard input is not available');
+        }
+
+        this.setCollideWorldBounds(true);
+
+        this.createAnimations();
+        this.play('idle');
+    }
+
+    createAnimations ()
+    {
+        this.scene.anims.create({
+            key: 'idle',
+            frames: this.scene.anims.generateFrameNumbers('cat', { start: 0, end: 3 }),
+            frameRate: 5,
+            repeat: -1
+        });
+    }
+
+    update ()
+    {
+        const speed = 200;
+
+        if (this.cursors.left?.isDown) {
+            this.setVelocityX(-speed);
+        } else if (this.cursors.right?.isDown) {
+            this.setVelocityX(speed);
+        } else {
+            this.setVelocityX(0);
+        }
+
+        if (this.cursors.up?.isDown) {
+            this.setVelocityY(-speed);
+        } else if (this.cursors.down?.isDown) {
+            this.setVelocityY(speed);
+        } else {
+            this.setVelocityY(0);
+        }
+    }
+}

26bf98b1419d50386d22a4671ee3beb01e087272
Deniss Suhhanov <desuhh@taltech.ee>
2025-02-24 17:01:19 +0200

    Make world larger than the scene, make camera follow the player

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -4,7 +4,7 @@ import { Player } from '../objects/Player';
 export class Game extends Scene
 {
     camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
+    background: Phaser.GameObjects.TileSprite;
     msg_text : Phaser.GameObjects.Text;
     ground: Phaser.Physics.Arcade.StaticGroup;
     player: Player;
@@ -16,28 +16,41 @@ export class Game extends Scene
 
     create ()
     {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+        const worldWidth = this.scale.width * 3;
+        this.physics.world.setBounds(0, 0, worldWidth, this.scale.height);
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+        this.player = new Player(this, worldWidth / 2, 384);
+
+        this.camera = this.cameras.main;
+        this.camera.setBackgroundColor('#00ff00');
+        this.camera.startFollow(this.player, true, 0.1, 0.1);
+        this.camera.setBounds(0, 0, worldWidth, this.scale.height);
+
+        this.background = this.add.tileSprite(0, 0, worldWidth, this.scale.height, 'background')
+            .setOrigin(0, 0)
+            .setScrollFactor(0);
 
         this.ground = this.physics.add.staticGroup();
-        for (let x = 0; x < this.scale.width; x += 48) {
-            this.ground.create(x, 744, 'terrain', 2);
+        for (let x = 0; x < worldWidth; x += 48) {
+            this.ground.create(x, 744, 'terrain', 2).setOrigin(0, 0.5);
         }
 
-        this.player = new Player(this, 512, 384);
+        this.player.setDepth(10);
 
         this.physics.add.collider(this.player, this.ground);
-
-        this.input.once('pointerdown', () => {
-            this.scene.start('GameOver');
-        });
     }
 
     update ()
     {
+        const playerX = this.player.x;
+        const camera = this.camera;
+        const screenWidth = this.scale.width;
+        const edgeThreshold = 200;
+
+        if (playerX > camera.scrollX + edgeThreshold && playerX < camera.scrollX + screenWidth - edgeThreshold) {
+            camera.scrollX = playerX - screenWidth / 2;
+        }
+
         this.player.update();
     }
 }
